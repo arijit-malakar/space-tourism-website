a399e6425a9d03df2d9dac5dfdb2d2b78bbf5c31
Arijit Malakar <arijitmalakar1996@gmail.com>
2023-11-13 21:56:56 +0530

    Initial commit - assets, base style

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+const App = () => {
+  return (
+    <>
+      <header>
+        <nav>00 Home 01 Destination 02 Crew 03 Technology</nav>
+      </header>
+      <main>
+        <h5>So, you want to travel to</h5>
+        <h1>Earth</h1>
+        <p>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </main>
+      Explore
+    </>
+  );
+};
+
+export default App;
